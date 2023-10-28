3ce1ffdfec3fe2af2f3a4d327b2338dcf8934eb6
inesucena <inesmsucena@ua.pt>
2023-10-28 17:31:28 +0100

    alteração do ficheiro next.congif.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    unoptimized: true,
+    unoptimized: true
   },
-  assetPrefix: "/",
-};
+  experimental: {
+    forceSwcTransforms: true,
+  },
+  assetPrefix: '/',
+}
